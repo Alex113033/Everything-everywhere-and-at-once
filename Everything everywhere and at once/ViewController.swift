a86f149f08b487dfd56e8c33d810a23490291fdc
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-11-15 20:14:23 +0300

    Initial Commit

---
+++ Everything everywhere and at once/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Everything everywhere and at once
+//
+//  Created by Александр on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
